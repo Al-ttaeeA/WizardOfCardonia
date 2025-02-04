aced733b1af11a38871cdf3a2ff954bf213946a0
Ahmed Al-Ttaee <ahmedalttaee0306@gmail.com>
2025-02-04 01:36:33 -0500

    Added Magical and Undead enemy classes

---
+++ WizardOfCardonia/src/enemy/MagicalEnemy.java
@@ -0,0 +1,30 @@
+package enemy;
+
+public class MagicalEnemy extends Enemy{
+	private double currentMult;
+	private final double increaseMult;
+	
+	public MagicalEnemy(String name, int health, int block, int blockAmount, int damageConstant, int damageVariable, double specialChance, double increaseMult) {
+		super(name, health, block, blockAmount, damageConstant, damageVariable, specialChance);
+		this.increaseMult = increaseMult;
+		this.currentMult = 1;
+	}
+	
+	public MagicalEnemy() {
+		super();
+		this.increaseMult = 0;
+		this.currentMult = 1;
+	}
+	
+	public void attack() {
+		if(game.Commands.getRandomChance() < specialChance) {
+			currentMult += increaseMult;
+		}
+		else if(game.Commands.getRandomChance() < 0.75) {
+			game.Main.testInt -= (int) (this.getDamage() * currentMult);
+		}
+		else {
+			block += (int) (blockAmount * currentMult);
+		}
+	}
+}
--- WizardOfCardonia/src/enemy/UndeadEnemy.java
+++ WizardOfCardonia/src/enemy/UndeadEnemy.java
@@ -1,18 +1,21 @@
 package enemy;
 
-public class HumanEnemy extends Enemy{
-	public HumanEnemy(String name, int health, int block, int blockAmount, int damageConstant, int damageVariable, double specialChance) {
+public class UndeadEnemy extends Enemy{
+	private final int healAmount;
+	
+	public UndeadEnemy(String name, int health, int block, int blockAmount, int damageConstant, int damageVariable, double specialChance, int healAmount) {
 		super(name, health, block, blockAmount, damageConstant, damageVariable, specialChance);
+		this.healAmount = healAmount;
 	}
 	
-	public HumanEnemy() {
+	public UndeadEnemy() {
 		super();
+		this.healAmount = 0;
 	}
 	
 	public void attack() {
 		if(game.Commands.getRandomChance() < specialChance) {
-			game.Main.testInt -= this.getDamage();
-			block += blockAmount;
+			health += healAmount;
 		}
 		else if(game.Commands.getRandomChance() < 0.75) {
 			game.Main.testInt -= this.getDamage();
